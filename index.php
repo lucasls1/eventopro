dadb306d29e5bf8969216f6b00ad415004f8f76e
Lucas <lucassntg28@gmail.com>
2019-04-30 23:43:09 -0300

    Inicio do projeto

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php 
+
+require_once("vendor/autoload.php");
+
+$app = new \Slim\Slim();
+
+$app->config('debug', true);
+
+$app->get('/', function() {
+    
+	echo "OK";
+	$sql= new EventoPro\DB\Sql();
+	$results = $sql->select("SELECT * FROM tb_usuario");
+	echo json_encode($results);
+});
+
+$app->run();
+
+ ?>
